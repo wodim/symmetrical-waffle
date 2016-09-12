3c88b3956b7c22af8963ee1555a94eb0ba51a66a
wodim <neikokz@gmail.com>
2016-09-12 14:06:46 +0200

    unfollow users in parallel too

--- followers.py
+++ followers.py
@@ -7,6 +7,21 @@ import tweepy
 from twitter import twitter
 
 
+class Parallel(object):
+    def __init__(self, func, things, num_threads=50):
+        self.queue = Queue()
+        for i in range(num_threads):
+            thread = threading.Thread(target=func, args=(self.queue,))
+            thread.daemon = True
+            thread.start()
+
+        for thing in things:
+            self.queue.put(thing)
+
+    def start(self):
+        self.queue.join()
+
+
 def follow_followers(screen_name=None, num_threads=50):
     screen_name = screen_name or twitter.me.screen_name
 
@@ -18,14 +33,14 @@ def follow_followers(screen_name=None, num_threads=50):
             if (not user.following and
                 not user.follow_request_sent and
                 user.id != twitter.me.id and
-                user.followers_count > 100 and
+                user.followers_count > 50 and
                 (hasattr(user, 'status') and
-                 user.status.created_at > datetime.now() - timedelta(days=3))):
+                 user.status.created_at > datetime.now() - timedelta(days=7))):
                 users.append(user)
         print('{len} users downloaded...'.format(len=len(users)))
     print('Finished: {len} users total'.format(len=len(users)))
 
-    def follow():
+    def follow(queue):
         while True:
             user = queue.get()
             while True:
@@ -43,30 +58,39 @@ def follow_followers(screen_name=None, num_threads=50):
                     break
             queue.task_done()
 
-    queue = Queue()
-    for i in range(num_threads):
-        thread = threading.Thread(target=follow)
-        thread.daemon = True
-        thread.start()
-
-    for user in users:
-        queue.put(user)
-    queue.join()
+    parallel = Parallel(follow, users)
+    parallel.start()
 
 
-def unfollow_my_unfollowers():
-    # 100 per page is the max lookup_friendships can do
-    for page in tweepy.Cursor(twitter.api.friends, count=100).pages():
-        user_ids = [user.id for user in page]
-        for user in twitter.api._lookup_friendships(user_ids):
-            if not user.is_followed_by:
-                print('Unfollowing @{screen_name} ({id})'
-                      .format(screen_name=user.screen_name,
-                              id=user.id))
-                try:
-                    twitter.api.destroy_friendship(user.id)
-                except tweepy.error.TweepError as e:
-                    print('Error unfollowing.')
+def mass_unfollow(only_unfollowers=False):
+    users = []
+    count = 100 if only_unfollowers else 200
+    for page in tweepy.Cursor(twitter.api.friends, count=count).pages():
+        if only_unfollowers:
+            user_ids = [user.id for user in page]
+            for user in twitter.api._lookup_friendships(user_ids):
+                if not user.is_followed_by:
+                    users.append(user)
+        else:
+            users.extend([user.screen_name for user in page])
+
+        print('{len} users downloaded...'.format(len=len(users)))
+    print('Finished: {len} users total'.format(len=len(users)))
+
+    def unfollow(queue):
+        while True:
+            user = queue.get()
+            print('Unfollowing @{screen_name} ({id})'
+                  .format(screen_name=user.screen_name,
+                          id=user.id))
+            try:
+                twitter.api.destroy_friendship(user.id)
+            except tweepy.error.TweepError as e:
+                print('Error unfollowing.')
+            queue.task_done()
+
+    parallel = Parallel(unfollow, users)
+    parallel.start()
 
 
 def get_list(screen_name=None, type='friends'):
