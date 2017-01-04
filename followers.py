591939fd8651ffc0e427ddadb7efce34dafc1917
wodim <neikokz@gmail.com>
2017-01-04 21:53:19 +0000

    added docstrings and made a few updates

--- followers.py
+++ followers.py
@@ -21,6 +21,8 @@ SOURCES_WHITELIST = ('Twitter for Android',
                      'Mobile Web',
                      'Mobile Web (M2)',
                      'Mobile Web (M5)')
+STATUS_INFO = ('{tpc:>6.1%} done. '
+               '{users:>5} users downloaded ({fpc:.1%} filtered)')
 STATUS_LEFT = '{left:> 6d} left | '
 FOLLOWING_PREFIX = 'Following @{screen_name} ({id}): '
 UNFOLLOWING_PREFIX = 'Unfollowing @{screen_name} ({id}): '
@@ -35,13 +37,14 @@ UNFOLLOW_SUCCESS = STATUS_LEFT + UNFOLLOWING_PREFIX + 'success!'
 class Parallel(object):
     def __init__(self, func, things, num_threads):
         self.queue = Queue()
+
+        for thing in things:
+            self.queue.put(thing)
+
         for _ in range(num_threads):
             thread = threading.Thread(target=func, args=(self.queue,))
             thread.daemon = True
             thread.start()
-
-        for thing in things:
-            self.queue.put(thing)
 
     def start(self):
         self.queue.join()
@@ -49,26 +52,52 @@ class Parallel(object):
 
 def mass_follow(screen_name=None, min_followers=50, last_post_delta=7,
                 not_my_followers=True, lang=None, num_threads=50,
-                type='followers'):
+                type='followers', check_eligibility=True):
+    """Massively follow a list of users taken from another account.
+
+    Args:
+        screen_name: screen name of the user we'll get the list from. can be
+            omitted, and then it defaults to yourself.
+        min_followers: minimum amount of followers a user has to have to be
+            followed.
+        last_post_delta: the user has to have posted something in the last X
+            days using one of the clients hardcoded in SOURCES_WHITELIST to be
+            followed. useful to ignore dormant/spammy accounts.
+        not_my_followers: don't follow users who already follow you.
+        lang: only follow users who use the twitter app/website in a certain
+            language, such as "en", "de", etc
+        num_threads: number of threads that will be launched to follow users
+            in parallel. doesn't affect the retrieval of the list.
+        type: list of users that we'll download from screen_name: can be either
+            his 'followers' or his 'friends' (following).
+        check_eligibility: check the eligibility of the user using
+            is_eligible() below. that function filters obvious spambots.
+    """
     if not screen_name:
         screen_name = twitter.me.screen_name
 
     if type == 'friends':
         func = twitter.api.friends
+        total = twitter.api.get_user(screen_name).friends_count
     elif type == 'followers':
         func = twitter.api.followers
+        total = twitter.api.get_user(screen_name).followers_count
     else:
         raise ValueError
     # download the list of users to follow
     users = []
+    all_users = []
     for page in tweepy.Cursor(func, screen_name=screen_name,
                               count=200).pages():
         for user in page:
+            all_users.append(user)
             if user.following or user.follow_request_sent:
                 continue
             if user.id == twitter.me.id:
                 continue
             if min_followers and user.followers_count < min_followers:
+                continue
+            if check_eligibility and not _is_eligible(user):
                 continue
             if lang and lang != user.lang:
                 continue
@@ -83,7 +112,9 @@ def mass_follow(screen_name=None, min_followers=50, last_post_delta=7,
                 if user.status.source not in SOURCES_WHITELIST:
                     continue
             users.append(user)
-        print('{len} users downloaded...'.format(len=len(users)))
+        print(STATUS_INFO.format(users=len(users),
+                                 tpc=len(all_users) / total,
+                                 fpc=len(users) / len(all_users)))
     print('Finished: {len} users total'.format(len=len(users)))
     random.shuffle(users)
 
@@ -118,6 +149,16 @@ def mass_follow(screen_name=None, min_followers=50, last_post_delta=7,
 
 
 def follow_from_file(filename, ids=False, num_threads=50):
+    """Massively follow a list of users taken from a file.
+
+    Args:
+        filename: name of the file.
+        ids: true if the list contains a list of ids (account numbers), false
+            if it's screen names instead.
+        num_threads: number of threads that will be launched to follow users
+            in parallel.
+
+    """
     with open(filename) as file:
         users = [line.rstrip('\n') for line in file]
 
@@ -132,14 +173,14 @@ def follow_from_file(filename, ids=False, num_threads=50):
 
             # first check if the user is eligible at all
             try:
-                if type(user) == int:
+                if isinstance(user, int):
                     user_obj = twitter.api.get_user(user_id=user)
-                elif type(user) == str:
+                elif isinstance(user, str):
                     user_obj = twitter.api.get_user(screen_name=user)
             except tweepy.error.TweepError:
                 queue.task_done()
                 continue
-            if not is_eligible(user_obj):
+            if not _is_eligible(user_obj):
                 print(FOLLOW_UNELIGIBLE.format(left=queue.qsize() + 1,
                                                screen_name=user, id=user))
                 queue.task_done()
@@ -147,9 +188,9 @@ def follow_from_file(filename, ids=False, num_threads=50):
 
             while True:
                 try:
-                    if type(user) == int:
+                    if isinstance(user, int):
                         twitter.api.create_friendship(user_id=user)
-                    elif type(user) == str:
+                    elif isinstance(user, str):
                         twitter.api.create_friendship(screen_name=user)
                 except tweepy.error.TweepError as exc:
                     if exc.api_code == 161:
@@ -174,6 +215,15 @@ def follow_from_file(filename, ids=False, num_threads=50):
 
 def mass_unfollow(only_followers=False, only_unfollowers=False,
                   num_threads=50):
+    """Massively unfollow users.
+
+    Args:
+        only_followers: only unfollow users who already follow you.
+        only_unfollowers: only unfollow users who don't follow you.
+        num_threads: number of threads that will be launched to unfollow users
+            in parallel.
+
+    """
     if only_followers and only_unfollowers:
         raise ValueError('choose either only_followers or only_unfollowers!')
 
@@ -218,6 +268,18 @@ def mass_unfollow(only_followers=False, only_unfollowers=False,
 
 
 def print_list(screen_name=None, type='friends', format='simple'):
+    """Print a list of users.
+
+    Args:
+        screen_name: screen name of the user we'll get the list from. can be
+            omitted, and then it defaults to yourself.
+        type: list of users that we'll download from screen_name: can be either
+            his 'followers' or his 'friends' (following).
+        format: can be either 'simple' (a list of screen names will be printed)
+            or 'csv' (a csv file with screen_name, number of friends and number
+            of followers will be printed).
+
+    """
     screen_name = screen_name or twitter.me.screen_name
 
     if format not in {'simple', 'csv'}:
@@ -243,13 +305,31 @@ def print_list(screen_name=None, type='friends', format='simple'):
                                     user.followers_count))
 
 
+def print_tweets(screen_name=None, include_retweets=False):
+    """Print a list of recent tweets.
+
+    Args:
+        screen_name: screen name of the user we'll get the list from. can be
+            omitted, and then it defaults to yourself.
+        include_retweets: whether retweets will be included in the list.
+
+    """
+    screen_name = screen_name or twitter.me.screen_name
+
+    for status in tweepy.Cursor(twitter.api.user_timeline,
+                                screen_name=screen_name).items():
+        if not include_retweets and hasattr(status, 'retweeted_status'):
+            continue
+        text = status.text.replace('\n', ' ').replace('\r', ' ')
+        print(text)
+
+
 # from akari
-def is_eligible(user):
+def _is_eligible(user):
     """checks if a user is eligible to be followed."""
     if user.friends_count > 5000:
         return False
     if (user.followers_count > 5000 and
             user.friends_count / user.followers_count > 0.7):
         return False
-
     return True
